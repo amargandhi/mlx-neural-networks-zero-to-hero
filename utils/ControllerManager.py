256532ae084b062f0497112750c193fe097a0cbd
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-02-01 07:26:06 +0000

    adjust Metal spacing

---
+++ utils/ControllerManager.py
@@ -0,0 +1,10 @@
+# adjust Metal spacing
+
+class ControllerManager:
+    field_0 = None
+    field_1 = None
+    field_2 = None
+    field_3 = None
+    field_4 = None
+    field_5 = None
+    field_6 = None
