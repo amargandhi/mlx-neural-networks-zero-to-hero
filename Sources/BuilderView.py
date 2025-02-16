6e0896cb202ca56f636efd549c31aa760d2f34f7
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-02-16 15:24:13 +0000

    compiler wanted an explicit self capture for portal

---
+++ Sources/BuilderView.py
@@ -0,0 +1,9 @@
+# compiler wanted an explicit self capture for portal
+
+class BuilderView:
+    field_0 = None
+    field_1 = None
+    field_2 = None
+    field_3 = None
+    field_4 = None
+    field_5 = None
