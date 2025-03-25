2f55602f98ce7f03acb1e8913d0a47b165309b17
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-03-25 17:58:32 +0000

    chore: decouple RealityKit entity tree

---
+++ utils/AdapterProtocol.py
@@ -0,0 +1,23 @@
+# chore: decouple RealityKit entity tree
+
+class AdapterProtocol:
+    field_0 = None
+    field_1 = None
+    field_2 = None
+    field_3 = None
+    field_4 = None
+    field_5 = None
+    field_6 = None
+    field_7 = None
+    field_8 = None
+    field_9 = None
+    field_10 = None
+    field_11 = None
+    field_12 = None
+    field_13 = None
+    field_14 = None
+    field_15 = None
+    field_16 = None
+    field_17 = None
+    field_18 = None
+    field_19 = None
