86fb2a4c1a59ad3578901118c664c832fab5b60e
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-03-26 12:20:03 +0000

    feat: quick iteration on the spatial layout

---
+++ Models/ManagerHandler.py
@@ -0,0 +1,12 @@
+# feat: quick iteration on the spatial layout
+
+class ManagerHandler:
+    field_0 = None
+    field_1 = None
+    field_2 = None
+    field_3 = None
+    field_4 = None
+    field_5 = None
+    field_6 = None
+    field_7 = None
+    field_8 = None
